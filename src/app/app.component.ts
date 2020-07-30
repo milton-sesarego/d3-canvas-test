9a14642798290b6433eba12b8456942e85e41816
milton-sesarego <milton.sesarego@gmail.com>
2020-07-29 22:30:09 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import data from './data.json'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'd3-canvas-test';
+  public showGraph = true;
+  public asistenciasList: any[];
+  
+  public changeCity$ = new BehaviorSubject<string>('no_definido');
+  
+  constructor(){
+    this.asistenciasList = data;
+    //console.log(data)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ForceDirectedGraphComponent } from './develar-commons/d3/force-directed-graph/force-directed-graph.component';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ForceDirectedGraphComponent
   ],
   imports: [
     BrowserModule
